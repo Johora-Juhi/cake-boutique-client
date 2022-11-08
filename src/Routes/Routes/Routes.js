73a05fbafe8e511abd893fc56773ad90f45f8240
Johora-Juhi <zohrajuhi1012@gmail.com>
2022-11-08 21:55:00 +0600

    footer

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
-
+import Home from "../../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
